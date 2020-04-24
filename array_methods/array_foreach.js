b3e4d2d6fae722d101dcf20b71b887ccbe9a8665
Chris Nguyen <chris@tnwebdev.com>
2020-04-24 04:20:17 -0700

    Add the array forEach method

--- array_methods/array_foreach.js
+++ array_methods/array_foreach.js
@@ -1,7 +1,13 @@
-// The traditional for loop
-
+// Declare colors array
 var colors = ['Brown', 'Red', 'Orange', 'Yellow', 'Green', 'Blue', 'Purple', 'Gray', 'White', 'Black'];
 
+// The traditional for loop
 for (var i = 0; i < colors.length; i++) {
    console.log(i, colors[i]);
 }
+
+// Array forEach() method
+colors.forEach(function (element, i) {
+   console.log(i, element);
+});
+
